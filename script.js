79677541c5ffb9a3e366b08f902a5dffc60e3870
1cpkazimour <1cpkazimour@noreply.github.com>
2023-06-03 12:45:12 -0700

    added json content generation

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ let data = fetch("/Parsed_Info.json").then(resp => {
     return resp.body.getReader().read();
 }).then(body => {
     let textDecoder = new TextDecoder("utf-8");
-    console.log(JSON.parse(textDecoder.decode(body.value)));
+    let plants = JSON.parse(textDecoder.decode(body.value)).invasive_species;
+
+    let dataTable = document.getElementById("data_table");
+
+    for(plant of plants) {
+        dataTable.insertAdjacentHTML('beforeend', `
+            <tr>
+                <td><img src="${plant.picture_url}"></td>
+                <td>${plant.common_name}</td>
+                <td>${plant.scientific_name}</td>
+                <td>${plant.impact}</td>
+            </tr>
+        `);
+    }
 });
