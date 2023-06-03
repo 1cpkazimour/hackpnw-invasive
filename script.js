b03a0fcfd3848d8819046c5e63890e7a03a9d006
1cpkazimour <1cpkazimour@noreply.github.com>
2023-06-03 12:29:55 -0700

    I don't understand how promises work :)

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ let data = fetch("/Parsed_Info.json").then(resp => {
     return resp.body.getReader().read();
 }).then(body => {
     let textDecoder = new TextDecoder("utf-8");
-    return JSON.parse(textDecoder.decode(body.value));
+    console.log(JSON.parse(textDecoder.decode(body.value)));
 });
-
-console.log(data)
