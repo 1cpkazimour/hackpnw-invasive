9256352dea645d45a6540bba0f9bd8a611158359
1cpkazimour <1cpkazimour@noreply.github.com>
2023-06-03 12:28:43 -0700

    made it better

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let data = fetch("/Parsed_Info.json").then(resp => {
     return resp.body.getReader().read();
 }).then(body => {
     let textDecoder = new TextDecoder("utf-8");
-    console.log(JSON.parse(textDecoder.decode(body.value)));
+    return JSON.parse(textDecoder.decode(body.value));
 });
 
 console.log(data)
