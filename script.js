a654689b2ebde6909f1802495749bd93143eb9b9
1cpkazimour <1cpkazimour@noreply.github.com>
2023-06-03 13:19:23 -0700

    picture_url is no longer necessary in Parsed_Info.json

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-let data = fetch("/Parsed_Info.json").then(resp => {
+let imageFilenamesPromise = fetch("/Image_Filenames.json").then(resp => {
     return resp.body.getReader().read();
 }).then(body => {
     let textDecoder = new TextDecoder("utf-8");
-    let plants = JSON.parse(textDecoder.decode(body.value)).invasive_species;
+    return JSON.parse(textDecoder.decode(body.value));
+})
 
+let plantsPromise = fetch("/Parsed_Info.json").then(resp => {
+    return resp.body.getReader().read();
+}).then(body => {
+    let textDecoder = new TextDecoder("utf-8");
+    return JSON.parse(textDecoder.decode(body.value)).invasive_species;
+});
+
+Promise.all([imageFilenamesPromise, plantsPromise]).then(([imageFilenames, plants]) => {
     let dataTable = document.getElementById("data_table");
-
+   
     for(plant of plants) {
+        let imageUrl = `https://plants.sc.egov.usda.gov/ImageLibrary/standard/${imageFilenames[plant.code]}`;
+        
         dataTable.insertAdjacentHTML('beforeend', `
             <tr>
-                <td><img src="${plant.picture_url}"></td>
+                <td><img src="${imageUrl}"></td>
                 <td>${plant.common_name}</td>
                 <td><i>${plant.scientific_name}</i></td>
                 <td>${plant.impact}</td>
             </tr>
         `);
     }
-});
+})
